80136ab921daceb8274eff8cd2f95349d9550183
Angela Pacheco <143752447+RAngelaPG@users.noreply.github.com>
2026-03-26 16:11:00 -0600

    Update runShinyApp.R
    Add line for upload big files

--- runShinyApp.R
+++ runShinyApp.R
@@ -1,5 +1,6 @@
 .libPaths("./R-Portable/App/R-Portable/library")
 message('library paths:\n', paste('... ', .libPaths(), sep='', collapse='\n'))
+options(shiny.maxRequestSize = 100 * 1024^2)  # 100 MB
 shiny::runApp("./shiny/",port=8888,launch.browser=TRUE)
 
 
